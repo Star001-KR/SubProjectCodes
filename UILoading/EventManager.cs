3cd038f4943aa7788bf0245f9ca0fec2e33136b6
Star001-KR <75716022+Star001-KR@users.noreply.github.com>
2021-09-06 13:06:45 +0900

    [김태형] EventManager 를 싱글턴 클래스로 생성

--- UILoading/EventManager.cs
+++ UILoading/EventManager.cs
@@ -1,1 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
+/*
+ * 2021.09.06 / code by Tae Hyung Kim.
+ */
+public enum EVENT_TYPE { LOADING };
+ 
+public class EventManager : MonoBehaviour {
+    private static EventManager _instance = null;
+    public static EventManager Instance
+    {
+        get { return _instance; }
+        set {}
+    }
+    
+    void Awake()
+    {
+        if (_instance == null)
+            _instance = this;
+            
+        else
+            DestroyImmediate(this);
+    }
+}
